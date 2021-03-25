d86f8edc0e4d8bac1455a084f2b8fa0d69e709c8
Andrés Herrera García <andreshg112@gmail.com>
2021-03-24 19:55:19 -0500

    Obtains the Colombian Holidays for 2020 #1

--- src/HolidaysPhp.php
+++ src/HolidaysPhp.php
@@ -2,7 +2,64 @@
 
 namespace Andreshg112\HolidaysPhp;
 
+use DOMXPath;
+use DOMDocument;
+
 class HolidaysPhp
 {
-    // Build your next great package.
+    public function all(): ?array
+    {
+        $dom = new DOMDocument();
+
+        @$dom->loadHTMLFile('https://publicholidays.co/es/2020-dates');
+
+        $finder = new DOMXPath($dom);
+
+        $classname = "publicholidays";
+
+        $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+
+        if (empty($nodes)) {
+            return null;
+        }
+
+        /** @var \DOMElement */
+        $table = $nodes->item(0);
+
+        $tbodyList = $table->getElementsByTagName('tbody');
+
+        if (empty($tbodyList)) {
+            return null;
+        }
+
+        $tbody = $tbodyList->item(0);
+
+        $trs = $tbody->childNodes;
+
+        if (empty($trs)) {
+            return null;
+        }
+
+        $holidays = [];
+
+        for ($i = 0; $i < $trs->count() - 1; $i++) {
+            $tr = $trs->item($i);
+
+            $tds = $tr->childNodes;
+
+            if ($tds->count() !== 3) {
+                continue;
+            }
+
+            $holiday = [
+                'date' => trim($tds->item(0)->textContent),
+                'day' => trim($tds->item(1)->textContent),
+                'name' => trim($tds->item(2)->textContent),
+            ];
+
+            $holidays[] = $holiday;
+        }
+
+        return $holidays;
+    }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,12 +3,19 @@
 namespace Andreshg112\HolidaysPhp\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Andreshg112\HolidaysPhp\HolidaysPhp;
 
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function it_returns_a_list()
     {
-        $this->assertTrue(true);
+        $object = new HolidaysPhp;
+
+        $holidays = $object->all();
+
+        $this->assertIsArray($holidays);
+
+        $this->assertCount(18, $holidays);
     }
 }
