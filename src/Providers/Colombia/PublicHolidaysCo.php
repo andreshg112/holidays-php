79cb309c1b5e6edad58b75e133978ac03e4910e0
Andrés Herrera García <andreshg112@gmail.com>
2021-03-27 17:15:48 -0500

    Travis disabled

--- src/Providers/Colombia/PublicHolidaysCo.php
+++ src/Providers/Colombia/PublicHolidaysCo.php
@@ -35,18 +35,8 @@ class PublicHolidaysCo extends BaseProvider
         $url = $this->getLanguage() === 'en' ? "{$baseUrl}/{$year}-dates" : "{$baseUrl}/es/{$year}-dates";
 
         try {
-            $context = stream_context_create(
-                array(
-                    "http" => array(
-                        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-                    )
-                )
-            );
-
-            $html = file_get_contents($url, false, $context);
+            $html = file_get_contents($url);
         } catch (\Throwable $th) {
-            var_dump($th->getMessage());
-
             throw HolidaysPhpException::notFound();
         }
 
